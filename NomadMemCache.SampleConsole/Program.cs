4fe93ca231810494808c566c7d8f7ef7b95d8686
melihmucuk <melihmucuk@gmail.com>
2015-03-27 16:40:45 +0200

    init

---
+++ NomadMemCache.SampleConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NomadMemCache.SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>{0, 1, 2, 3};
+
+            var cacher = Cacher.Instance.Add("sampleKey", list, DateTimeOffset.Now.AddMinutes(5));
+            if (cacher)
+            {
+                var listFromCache = Cacher.Instance.GetValue<List<int>>("sampleKey");
+                foreach (var item in listFromCache)
+                {
+                    Console.WriteLine(item);
+                }
+                Cacher.Instance.Delete("sampleKey");
+            }            
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ NomadMemCache/Cacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace NomadMemCache
+{
+    public class Cacher
+    {
+        public static readonly Cacher Instance = new Cacher();
+        private static readonly MemoryCache Cache = MemoryCache.Default;
+
+        Cacher(){}
+
+        public T GetValue<T>(string key)
+        {
+            return (T)Cache.Get(key);
+        }
+
+        public bool Add(string key, object value, DateTimeOffset absExpiration)
+        {
+            return Cache.Add(key, value, absExpiration);
+        }
+        
+        public void Delete(string key)
+        {
+            if (Cache.Contains(key))
+                Cache.Remove(key);
+        }
+
+        
+    }
+}
